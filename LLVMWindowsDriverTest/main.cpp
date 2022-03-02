73fd038179290ea022eb222d59598514fd180193
gmh5225 <2315157@qq.com>
2022-03-02 15:19:11 +0800

    Update code

---
+++ LLVMWindowsDriverTest/main.cpp
@@ -0,0 +1,26 @@
+#include <fltKernel.h>
+#include <intrin.h>
+
+#define dprintf(...)                                                           \
+  DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, __VA_ARGS__)
+
+EXTERN_C
+NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+  dprintf("new wolrd!\n");
+
+  // test readcr3
+  {
+    auto cr3 = __readcr3();
+    dprintf("readcr3 cr3=%p\n", cr3);
+  }
+
+  // test writecr3
+  {
+    auto cr3 = __readcr3();
+    __writecr3(cr3);
+    dprintf("readcr3 cr3=%p\n", cr3);
+  }
+
+  return STATUS_VIRUS_INFECTED;
+}
